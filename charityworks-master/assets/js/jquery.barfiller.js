bd01563b9657b1d1f1a028b05a3e7ed747930602
thabitor <thabithamror@gmail.com>
2023-10-26 22:13:20 +0200

    Screenshot 1

--- charityworks-master/assets/js/jquery.barfiller.js
+++ charityworks-master/assets/js/jquery.barfiller.js
@@ -14,7 +14,7 @@
     $.fn.barfiller = function (options) {
 
         var defaults = $.extend({
-            barColor: '#09CC7F',
+            barColor: '#D82D33',
             tooltip: true,
             duration: 1000,
             animateOnResize: true,
